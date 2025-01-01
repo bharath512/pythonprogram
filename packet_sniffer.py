cadb0e8043ca434577c5be3a5819cc1acc6deb0c
bharath512 <67639106+bharath512@users.noreply.github.com>
2025-01-01 09:50:22 +0530

    Create packet_sniffer.py
    Packet sniffer python tool used to capture information from network packets

---
+++ packet_sniffer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+import optparse
+import scapy.all as scapy
+from scapy.layers import http
+
+def get_arguments():
+    parser=optparse.OptionParser()
+    parser.add_option("-i","--interface",dest="interface",help="Provide interface name to capture packets")
+    (options, arguments) = parser.parse_args()
+    if not (options.interface):
+        parser.error("Enter interface name to continue. Use --help for more info")
+    return options
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=sniffed_packets)
+
+def sniffed_packets(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            load = (packet[scapy.Raw].load.decode())
+            keywords = ["username", "uname", "user", "password", "pass", "login"]
+            for keyword in keywords:
+                if keyword in load:
+                    print(load)
+                    break
+
+options = get_arguments()
+sniff(options.interface)
